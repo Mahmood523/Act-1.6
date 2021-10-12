cce4bc98e5fae8b97b73f144c1e0e0c499f78084
Mahmood523 <91906977+Mahmood523@users.noreply.github.com>
2021-10-12 21:54:12 +0200

    Add files via upload

---
+++ MusiqueImpl.java
@@ -0,0 +1,79 @@
+
+public class MusiqueImpl implements Musique {
+	
+	public String titre;
+	public String autheur;
+	public String interprete;
+	public String genre;
+	public boolean verif;
+	
+	public MusiqueImpl(String titre, String autheur, String interprete, String genre) {
+		super();
+		this.titre = titre;
+		this.autheur = autheur;
+		this.interprete = interprete;
+		this.genre = genre;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAutheur() {
+		return autheur;
+	}
+
+	public void setAutheur(String autheur) {
+		this.autheur = autheur;
+	}
+
+	public String getInterprete() {
+		return interprete;
+	}
+
+	public void setInterprete(String interprete) {
+		this.interprete = interprete;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public boolean isVerif() {
+		return verif;
+	}
+
+	public void setVerif(boolean verif) {
+		this.verif = verif;
+	}
+	
+	
+
+	@Override
+	public String affichMusqiue(String titre,String autheur, String interprete, String genre) {
+		
+		System.out.println("Le titre : "+titre+" L'autheur : "+autheur+"L'interprete :"+interprete+" Le genre : "+genre);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean verifMusique(Musique m1, Musique m2) {
+		if   (m1==m2)
+		     verif=true;
+		else verif = false;	
+		return verif;
+	}
+
+	
+	
+
+}
---
+++ PlayListe.java
@@ -0,0 +1,9 @@
+
+public interface PlayListe {
+  
+   public abstract void aff_Mus_Play();
+   public abstract void add_Mus_Play();
+   public abstract void remove_Mus_Play();
+   public abstract void tri_Auteur();
+   public abstract void tri_Interprete();
+}
